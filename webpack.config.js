5a3a6df1fcbf784ac6792b2dbbfc831d657a8b70
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-05-31 19:10:56 +1000

    :(

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
-    resolve: {
-      "crypto": false,
-    }
     resolve.fallback: {
       "crypto": require.resolve("crypto-browserify"),
       "fs": false,
