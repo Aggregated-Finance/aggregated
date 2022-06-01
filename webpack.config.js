443bf23ca1c1389d5c2f022248abcaff32f43d0e
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-06-01 13:49:19 +1000

    webpack for like websuckasspack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     resolve: {
-      "crypto": false,
+      "crypto": false,//require.resolve("crypto-browserify"),
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,//require.resolve("stream-http"),
+      "https": false,
+      "stream": false,
     },
     resolve.fallback: {
       "crypto": false,//require.resolve("crypto-browserify"),
